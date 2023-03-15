8a87ddd977783539fd093151a7309f7de049cabb
Ward <wchamberlain89@gmail.com>
2023-03-15 11:29:53 -0700

    Initial Commit - Supabase Migration & Prisma Connected with Demo Model

---
+++ prisma/migrations/20230310235455_add_counter_widget/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropTable
+DROP TABLE "Post";
+
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "CounterWidget" (
+    "id" SERIAL NOT NULL,
+    "count" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "CounterWidget_pkey" PRIMARY KEY ("id")
+);
